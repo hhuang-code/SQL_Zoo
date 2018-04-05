0f1b2b26242c786e9af8fafacdc98fcef101f969
huanghao-code <huanghao_bupt@sina.com>
2018-04-05 12:42:43 -0400

    Create 1.sql

---
+++ 1.sql
@@ -0,0 +1,8 @@
+-- Q7: Find the countries that have three or more a in the name.
+select name from world where name like '%a%a%a%';
+
+-- Q8: Find the countries that have "t" as the second character.
+select name from world where name like '_t%';
+
+-- Q10: Find the countries that have exactly four characters.
+select name from world where name like '____';
