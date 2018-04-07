342da6e8564638c589ccdad42a3afb0c13d1930c
huanghao-code <huanghao_bupt@sina.com>
2018-04-06 20:50:07 -0400

    Update and rename 3.q to 3.sql

--- 3.sql
+++ 3.sql
@@ -11,3 +11,11 @@ select * from nobel where yr = 1980 and not (subject in ('Chemistry', 'Medicine'
 
 -- Q12: Find all details of the prize won by EUGENE O'NEILL
 select * from nobel where winner = 'Eugene O''Neill';
+
+-- Q13: List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+select winner, yr, subject from nobel where winner like 'Sir%' order by yr desc, winner;
+
+-- Q14: Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+select winner, subject from nobel where yr = 1984 order by subject in ('Chemistry', 'Physics'), subject, winner;
+
+
